1b6f487561d01089a878124427ebb32e9b5e75dc
msdkarimi <itsmasoudkarimi@gmail.com>
2023-12-27 02:47:00 +0100

    update code structure

--- model/__init__.py
+++ model/__init__.py
@@ -1,2 +1,6 @@
+from encoder.encoder import Encoder
+from decoder.decoder import Decoder
+import torch
+import torch.nn as nn
 
 
--- model/transformer_model.py
+++ model/transformer_model.py
@@ -1,7 +1,4 @@
-from encoder.encoder import Encoder
-from decoder.decoder import Decoder
-import torch
-import torch.nn as nn
+from model import *
 
 
 class Transformer(nn.Module):
